3398c3d11a2fc782b68234fdd80e9764deb3e52e
Shelane French <macwebdev-dev@yahoo.com>
2020-03-05 13:10:37 -0800

    Drupal 8.8.3

--- docroot/vendor/autoload.php
+++ docroot/vendor/autoload.php
@@ -4,4 +4,4 @@
 
 require_once __DIR__ . '/composer/autoload_real.php';
 
-return ComposerAutoloaderInitf58dd9d2e64a825de7df7497c1354a33::getLoader();
+return ComposerAutoloaderInitef05fc1afe55d667592a67cbca75d3bb::getLoader();
